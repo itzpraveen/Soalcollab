b0da34e499e13c433a561343a6080fd5cae5cac9
itzvpraveen <itzvpraveen@gmail.com>
2025-06-20 13:24:55 +0530

    fix: Use local SQLite database by default for development

--- studiohub/settings.py
+++ studiohub/settings.py
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'studiohub.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=env('DATABASE_URL'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+if 'DATABASE_URL' in env:
+    DATABASES['default'] = dj_database_url.config(
         conn_max_age=600
     )
-}
 
 
 # Password validation
